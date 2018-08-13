9147738a4dd03c7b4e7d6bfadef9956562108a09
Miriam Castillo <castillo.m.mc@gmail.com>
2018-08-13 05:47:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,14 @@ end
   end
 
   def self.artist_count
-    @@artist_count.uniq
+    @@artists.each do |genre|
+      if @@genre_count[genre]
+        @@genre_count[genre] += 1
+      else
+        @@genre_count[genre] = 1
+      end
+    end
+    @@genre_count
   end
 
 end
